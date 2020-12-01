aa85d8b499116a5414cb8926606d9068b34523f0
yeahji7 <67049211+yeahji7@users.noreply.github.com>
2020-12-01 16:59:16 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
-package chap04.exam05.over;
+package chap04.exam01.classDefine;
 
-public class Main {
 
-	public static void main(String[] args) {
-	//생성자는 한 클래스에 하나만 존재해야함.
-		
-		Robot r1= new Robot(); 
-		Robot r2= new Robot("android_001");
-		Robot r3= new Robot("android_001","청소");
-		
-		//NoteBook이라는 클래스 제작
-		//모델, 가격, 색상 필드 
-		// 이것들을 이용한  최대한 많은 생성자 오버로드를 만들어보자
-	}
+public class Main { 
+
 	
+	public static void main(String[] args) {
+	
+		//class(Student)를 복사해서 class(Student) 형태의 std1 변수에 담는다
+		Student std1 =new Student();
+		
+		//class 복사해서 std2에 만들기
+		Student std2= new Student();
+		System.out.println(std1);
+		System.out.println(std2);
+		System.out.println(std1.equals(std2));
 
+}
 }
